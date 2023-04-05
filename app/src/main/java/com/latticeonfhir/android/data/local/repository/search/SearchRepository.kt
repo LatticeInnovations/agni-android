2447affb5113716830147aea74a4a3c37ecc88d8
Naveen <naveen.s1605@gmail.com>
2023-04-05 12:58:07 +0530

    Dynamic Fuzzy Search

--- app/src/main/java/com/latticeonfhir/android/data/local/repository/search/SearchRepository.kt
+++ app/src/main/java/com/latticeonfhir/android/data/local/repository/search/SearchRepository.kt
@@ -2,9 +2,10 @@ package com.latticeonfhir.android.data.local.repository.search
 
 import androidx.lifecycle.LiveData
 import androidx.paging.PagingData
+import com.latticeonfhir.android.data.local.model.SearchParameters
 import com.latticeonfhir.android.data.server.model.patient.PatientResponse
 
 interface SearchRepository {
 
-    suspend fun searchPatients(query: String): LiveData<PagingData<PatientResponse>>
+    suspend fun searchPatients(searchParameters: SearchParameters): LiveData<PagingData<PatientResponse>>
 }
