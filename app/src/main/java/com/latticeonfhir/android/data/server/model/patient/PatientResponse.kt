425f9b39b7c955b05b0021d789ea1e1cfa281a87
mansikalraa <mansi.kalra23@gmail.com>
2023-04-07 16:07:18 +0530

    Date to LocalDate

--- app/src/main/java/com/latticeonfhir/android/data/server/model/patient/PatientResponse.kt
+++ app/src/main/java/com/latticeonfhir/android/data/server/model/patient/PatientResponse.kt
@@ -1,6 +1,7 @@
 package com.latticeonfhir.android.data.server.model.patient
 
 import com.latticeonfhir.android.base.baseclass.ParcelableClass
+import java.time.LocalDate
 import java.util.Date
 
 data class PatientResponse(
@@ -11,7 +12,7 @@ data class PatientResponse(
     val identifier: List<PatientIdentifier>,
     val active: Boolean?,
     val gender: String,
-    val birthDate: Date,
+    val birthDate: LocalDate,
     val mobileNumber: Long?,
     val email: String?,
     val permanentAddress: PatientAddressResponse,
