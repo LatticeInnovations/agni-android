c5b4b037e9a05c0017d29e99665282999ef6e450
Naveen <naveen.s1605@gmail.com>
2023-04-11 10:30:40 +0530

    Manage Relations in Add related beneficiary

--- app/src/main/java/com/latticeonfhir/android/data/local/repository/relation/RelationRepository.kt
+++ app/src/main/java/com/latticeonfhir/android/data/local/repository/relation/RelationRepository.kt
@@ -1,6 +1,6 @@
 package com.latticeonfhir.android.data.local.repository.relation
 
-import com.latticeonfhir.android.data.local.enums.RelationEnum
+import com.latticeonfhir.android.data.local.model.RelationBetween
 import com.latticeonfhir.android.data.local.roomdb.entities.RelationEntity
 import com.latticeonfhir.android.data.server.model.relatedperson.Relationship
 
@@ -8,7 +8,9 @@ interface RelationRepository {
 
     suspend fun addRelation(relationship: Relationship): List<Long>
     suspend fun addListOfRelation(listOfRelations: List<RelationEntity>): List<Long>
-    suspend fun getRelationBetween(fromId: String, toId: String): RelationEnum
+    suspend fun getRelationBetween(fromId: String, toId: String): RelationBetween
     suspend fun getAllRelationOfPatient(patientId: String): List<RelationEntity>
     suspend fun deleteRelation(vararg relationId: String): Int
+    suspend fun deleteRelation(fromId: String, toId: String): Int
+    suspend fun deleteAllRelationOfPatient(patientId: String): Int
 }
