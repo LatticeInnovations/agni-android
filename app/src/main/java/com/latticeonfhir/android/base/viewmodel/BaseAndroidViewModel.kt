94aa5a3bdc137691b89de982dde4a6f0ca3e711b
Naveen <naveen.s1605@gmail.com>
2023-04-11 12:41:51 +0530

    Conveter class to get relation

--- app/src/main/java/com/latticeonfhir/android/base/viewmodel/BaseAndroidViewModel.kt
+++ app/src/main/java/com/latticeonfhir/android/base/viewmodel/BaseAndroidViewModel.kt
@@ -2,8 +2,5 @@ package com.latticeonfhir.android.base.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import com.latticeonfhir.android.base.viewmodel.ParentViewModel
 
-open class BaseAndroidViewModel(application: Application): AndroidViewModel(application),
-    ParentViewModel {
-}
+open class BaseAndroidViewModel(application: Application): AndroidViewModel(application), ParentViewModel
