3b46ab16e3f5668f257af7979cc07a926804d7ab
mansikalraa <mansi.kalra23@gmail.com>
2023-04-17 12:42:21 +0530

    confirm relationship ui binding

--- app/src/main/java/com/latticeonfhir/android/data/local/repository/relation/RelationRepository.kt
+++ app/src/main/java/com/latticeonfhir/android/data/local/repository/relation/RelationRepository.kt
@@ -9,7 +9,7 @@ interface RelationRepository {
 
     suspend fun addRelation(relation: Relation): List<Long>
     suspend fun addListOfRelation(listOfRelations: List<RelationEntity>): List<Long>
-    suspend fun getRelationBetween(fromId: String, toId: String): LiveData<List<RelationView>>
+    suspend fun getRelationBetween(fromId: String, toId: String): List<RelationView>
     suspend fun getAllRelationOfPatient(patientId: String): List<RelationEntity>
     suspend fun deleteRelation(vararg relationId: String): Int
     suspend fun deleteRelation(fromId: String, toId: String): Int
--- app/src/main/java/com/latticeonfhir/android/ui/householdmember/suggestions/SuggestionsScreenViewModel.kt
+++ app/src/main/java/com/latticeonfhir/android/ui/householdmember/suggestions/SuggestionsScreenViewModel.kt
@@ -5,11 +5,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewModelScope
 import com.latticeonfhir.android.base.viewmodel.BaseViewModel
+import com.latticeonfhir.android.data.local.enums.GenericTypeEnum
+import com.latticeonfhir.android.data.local.model.Relation
 import com.latticeonfhir.android.data.local.model.SearchParameters
+import com.latticeonfhir.android.data.local.repository.generic.GenericRepository
+import com.latticeonfhir.android.data.local.repository.relation.RelationRepository
 import com.latticeonfhir.android.data.local.repository.search.SearchRepository
+import com.latticeonfhir.android.data.local.roomdb.dao.PatientDao
 import com.latticeonfhir.android.data.server.model.patient.PatientAddressResponse
 import com.latticeonfhir.android.data.server.model.patient.PatientIdentifier
 import com.latticeonfhir.android.data.server.model.patient.PatientResponse
+import com.latticeonfhir.android.data.server.model.relatedperson.RelatedPersonResponse
+import com.latticeonfhir.android.data.server.model.relatedperson.Relationship
+import com.latticeonfhir.android.utils.converters.responseconverter.toRelationEntity
+import com.latticeonfhir.android.utils.relation.RelationConverter
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -17,13 +26,20 @@ import java.util.concurrent.SynchronousQueue
 import javax.inject.Inject
 
 @HiltViewModel
-class SuggestionsScreenViewModel @Inject constructor(private val searchRepository: SearchRepository) :
+class SuggestionsScreenViewModel @Inject constructor(
+    private val searchRepository: SearchRepository,
+    private val genericRepository: GenericRepository,
+    private val relationRepository: RelationRepository,
+    private val patientDao: PatientDao
+    ) :
     BaseViewModel() {
     var showConnectDialog by mutableStateOf(false)
 
     private val queue = SynchronousQueue<PatientResponse>()
     private var i = 0
-    private lateinit var listOfSuggestions: List<PatientResponse>
+
+    //private lateinit var listOfSuggestions: List<PatientResponse>
+    var listOfSuggestions by mutableStateOf(listOf<PatientResponse>())
 
     val patient = PatientResponse(
         id = "d138ada3-82f7-4b96-914f-decd5933b61d",
@@ -56,7 +72,6 @@ class SuggestionsScreenViewModel @Inject constructor(private val searchRepositor
 
     val suggestedMembersList = listOf(patient, patient, patient)
 
-    //var suggestedMembersList by mutableStateOf(listOf<PatientResponse>())
 
     internal fun getQueueItems() {
         while (queue.size < 5) {
@@ -78,8 +93,45 @@ class SuggestionsScreenViewModel @Inject constructor(private val searchRepositor
         viewModelScope.launch(Dispatchers.IO) {
             listOfSuggestions = searchRepository.getSuggestedMembers(
                 patient.id,
-                SearchParameters("")
+                SearchParameters(
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    patient.permanentAddress.addressLine1,
+                    patient.permanentAddress.city,
+                    patient.permanentAddress.district,
+                    patient.permanentAddress.state,
+                    patient.permanentAddress.postalCode,
+                    patient.permanentAddress.addressLine2
+                )
             )
+        }
+    }
+
+    fun addRelation(relation: Relation, relativeId: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            RelationConverter.getInverseRelation(relation.toRelationEntity(), patientDao) {
+                viewModelScope.launch(Dispatchers.IO) {
+                    genericRepository.insertOrUpdatePostEntity(
+                        patientId = relation.patientId,
+                        entity = RelatedPersonResponse(
+                            id = relation.patientId,
+                            relationship = listOf(
+                                Relationship(
+                                    patientIs = RelationConverter.getRelationEnumFromString(relation.relation),
+                                    relativeId = relativeId,
+                                    relativeIs = it.value
+                                )
+                            )
+                        ),
+                        typeEnum = GenericTypeEnum.RELATION
+                    )
+                }
+            }
+            relationRepository.addRelation(relation)
         }
     }
 }
