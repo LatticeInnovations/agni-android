ddd71c2ade2bd188cad254b664c4276e26a940da
Naveen <naveen.s1605@gmail.com>
2023-04-18 20:38:41 +0530

    Worker Recursive call fix

--- app/src/main/java/com/latticeonfhir/android/service/workmanager/workers/base/SyncWorker.kt
+++ app/src/main/java/com/latticeonfhir/android/service/workmanager/workers/base/SyncWorker.kt
@@ -2,6 +2,7 @@ package com.latticeonfhir.android.service.workmanager.workers.base
 
 import android.content.Context
 import androidx.work.CoroutineWorker
+import androidx.work.ListenableWorker
 import androidx.work.WorkerParameters
 import com.latticeonfhir.android.data.server.repository.sync.SyncRepository
 
