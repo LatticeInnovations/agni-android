c879522fc024838b7f853c41c6b7792f4f5f0d62
mansikalraa <mansi.kalra23@gmail.com>
2023-04-27 00:16:56 +0530

    dao tests

--- app/src/androidTest/java/com/latticeonfhir/android/room_database/BaseClass.kt
+++ app/src/androidTest/java/com/latticeonfhir/android/room_database/BaseClass.kt
@@ -2,10 +2,15 @@ package com.latticeonfhir.android.room_database
 
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
+import com.latticeonfhir.android.data.local.enums.RelationEnum
 import com.latticeonfhir.android.data.local.roomdb.FhirAppDatabase
 import com.latticeonfhir.android.data.local.roomdb.dao.GenericDao
 import com.latticeonfhir.android.data.local.roomdb.dao.IdentifierDao
 import com.latticeonfhir.android.data.local.roomdb.dao.PatientDao
+import com.latticeonfhir.android.data.local.roomdb.dao.RelationDao
+import com.latticeonfhir.android.data.local.roomdb.dao.SearchDao
+import com.latticeonfhir.android.data.local.roomdb.entities.RelationEntity
+import com.latticeonfhir.android.data.local.roomdb.views.RelationView
 import com.latticeonfhir.android.data.server.model.patient.PatientAddressResponse
 import com.latticeonfhir.android.data.server.model.patient.PatientIdentifier
 import com.latticeonfhir.android.data.server.model.patient.PatientResponse
@@ -21,13 +26,25 @@ open class BaseClass {
     lateinit var patientDao: PatientDao
     lateinit var genericDao: GenericDao
     lateinit var identifierDao: IdentifierDao
+    lateinit var relationDao: RelationDao
+    lateinit var searchDao: SearchDao
 
     val id = UUIDBuilder.generateUUID()
+    val relativeId = UUIDBuilder.generateUUID()
+    val relationEntityId = UUIDBuilder.generateUUID()
+
+    val relationEntity = RelationEntity(
+        id = relationEntityId,
+        fromId = id,
+        toId = relativeId,
+        relation = RelationEnum.SPOUSE
+    )
+
     val patientIdentifier = PatientIdentifier(
-            identifierNumber = "PATIENT123",
-            identifierType = "https//patient.id//.com",
-            code = null
-        )
+        identifierNumber = "PATIENT123",
+        identifierType = "https//patient.id//.com",
+        code = null
+    )
     val patientResponse = PatientResponse(
         id = id,
         firstName = "Test",
@@ -51,6 +68,43 @@ open class BaseClass {
         identifier = listOf(patientIdentifier)
     )
 
+    val relative = PatientResponse(
+        id = relativeId,
+        firstName = "Relative",
+        middleName = null,
+        lastName = null,
+        birthDate = Date(469823400000).time.toPatientDate(),
+        email = "test@gmail.com",
+        active = true,
+        gender = "male",
+        mobileNumber = 9876543210,
+        fhirId = null,
+        permanentAddress = PatientAddressResponse(
+            postalCode = "111111",
+            state = "Uttarakhand",
+            addressLine1 = "H-123",
+            addressLine2 = "Jagjeetpur",
+            city = "Haridwar",
+            country = "India",
+            district = null
+        ),
+        identifier = listOf(patientIdentifier)
+    )
+
+    val relationView = RelationView(
+        id = relationEntityId,
+        patientFirstName = patientResponse.firstName,
+        patientGender = patientResponse.gender,
+        patientId = id,
+        patientLastName = patientResponse.lastName,
+        patientMiddleName = patientResponse.middleName,
+        relation = RelationEnum.SPOUSE,
+        relativeFirstName = relative.firstName,
+        relativeId = relativeId,
+        relativeLastName = relative.lastName,
+        relativeMiddleName = relative.middleName
+    )
+
     @Before
     fun setUp() {
         fhirAppDatabase = Room.inMemoryDatabaseBuilder(
@@ -60,6 +114,8 @@ open class BaseClass {
         patientDao = fhirAppDatabase.getPatientDao()
         genericDao = fhirAppDatabase.getGenericDao()
         identifierDao = fhirAppDatabase.getIdentifierDao()
+        searchDao = fhirAppDatabase.getSearchDao()
+        relationDao = fhirAppDatabase.getRelationDao()
     }
 
 
