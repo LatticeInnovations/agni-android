94bfb2d6fb74ed973dd1bd42ab248c89032e4f6a
mansikalraa <mansi.kalra23@gmail.com>
2023-04-28 16:53:36 +0530

    patient repository tests

---
+++ app/src/test/java/com/latticeonfhir/android/repository/PatientRepositoryTest.kt
@@ -0,0 +1,52 @@
+package com.latticeonfhir.android.repository
+
+import com.latticeonfhir.android.data.local.repository.patient.PatientRepositoryImpl
+import com.latticeonfhir.android.data.local.roomdb.dao.PatientDao
+import com.latticeonfhir.android.data.server.model.patient.PatientAddressResponse
+import com.latticeonfhir.android.data.server.model.patient.PatientIdentifier
+import com.latticeonfhir.android.data.server.model.patient.PatientResponse
+import com.latticeonfhir.android.utils.builders.UUIDBuilder
+import com.latticeonfhir.android.utils.converters.responseconverter.TimeConverter.toPatientDate
+import com.latticeonfhir.android.utils.converters.responseconverter.toPatientAndIdentifierEntityResponse
+import com.latticeonfhir.android.utils.converters.responseconverter.toPatientEntity
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
+import java.util.Date
+
+class PatientRepositoryTest: BaseClass() {
+    @Mock
+    private lateinit var patientDao: PatientDao
+    lateinit var patientRepositoryImpl: PatientRepositoryImpl
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        patientRepositoryImpl = PatientRepositoryImpl(patientDao)
+    }
+
+    @Test
+    fun addPatientTest() = runBlocking{
+        `when`(patientDao.insertPatientData(patientResponse.toPatientEntity())).thenReturn(listOf<Long>(-1))
+        val actual = patientRepositoryImpl.addPatient(patientResponse)
+        Assert.assertEquals(listOf<Long>(-1), actual)
+    }
+
+    @Test
+    fun updatePatientDataTest() = runBlocking{
+        `when`(patientDao.updatePatientData(patientResponse.toPatientEntity())).thenReturn(1)
+        val actual = patientRepositoryImpl.updatePatientData(patientResponse)
+        Assert.assertEquals(1, actual)
+    }
+
+    @Test
+    fun getPatientByIdTest() = runBlocking{
+        `when`(patientDao.getPatientDataById(id)).thenReturn(listOf(patientResponse.toPatientAndIdentifierEntityResponse()))
+        val actualUser = patientRepositoryImpl.getPatientById(id)
+        Assert.assertEquals(listOf(patientResponse), actualUser)
+    }
+}
