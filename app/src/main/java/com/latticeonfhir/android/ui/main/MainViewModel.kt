f23f000f1d1cb6c64328f8cec8d5b97a062b9f72
Hâwk <naveen.s1605@gmail.com>
2023-05-12 14:06:42 +0530

    Sync Repo initialization at MainViewModel

--- app/src/main/java/com/latticeonfhir/android/ui/main/MainViewModel.kt
+++ app/src/main/java/com/latticeonfhir/android/ui/main/MainViewModel.kt
@@ -1,8 +1,15 @@
 package com.latticeonfhir.android.ui.main
 
+import com.latticeonfhir.android.FhirApp
 import com.latticeonfhir.android.base.viewmodel.BaseViewModel
+import com.latticeonfhir.android.data.server.repository.sync.SyncRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : BaseViewModel()
+class MainViewModel @Inject constructor(syncRepository: SyncRepository) : BaseViewModel() {
+
+    init {
+        FhirApp.syncRepository = syncRepository
+    }
+}
