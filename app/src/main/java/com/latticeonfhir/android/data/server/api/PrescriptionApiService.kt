64fe7748c0369e348492f17c4cab5ea69160a183
Hâwk <naveen.s1605@gmail.com>
2023-05-26 11:50:10 +0530

    Add APIs for prescription

--- app/src/main/java/com/latticeonfhir/android/data/server/api/PrescriptionApiService.kt
+++ app/src/main/java/com/latticeonfhir/android/data/server/api/PrescriptionApiService.kt
@@ -1,7 +1,27 @@
 package com.latticeonfhir.android.data.server.api
 
+import com.latticeonfhir.android.base.server.BaseResponse
+import com.latticeonfhir.android.data.server.model.create.CreateResponse
+import com.latticeonfhir.android.data.server.model.prescription.medication.MedicationResponse
+import com.latticeonfhir.android.data.server.model.prescription.medication.MedicineTimeResponse
+import com.latticeonfhir.android.data.server.model.prescription.prescriptionresponse.PrescriptionResponse
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.QueryMap
 
 interface PrescriptionApiService {
 
+    @GET("Medication")
+    suspend fun getAllMedications(): Response<BaseResponse<List<MedicationResponse>>>
+
+    @POST("sync/{endPoint}")
+    suspend fun postPrescriptionRelatedData(@Path("endPoint") endPoint: String, prescriptionData: List<Any>): Response<BaseResponse<List<CreateResponse>>>
+
+    @GET("MedicationRequest")
+    suspend fun getPastPrescription(@QueryMap(encoded = true) map: Map<String,String>?): Response<BaseResponse<List<PrescriptionResponse>>>
+
+    @GET("medTime")
+    suspend fun getMedicineTime(): Response<BaseResponse<List<MedicineTimeResponse>>>
 }
