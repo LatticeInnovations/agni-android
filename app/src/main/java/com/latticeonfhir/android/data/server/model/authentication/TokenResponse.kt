3583448610d85a052e8466f34524a9f9f74afc23
mansikalraa <mansi.kalra23@gmail.com>
2023-05-26 16:30:16 +0530

    bug fixed : https://github.com/LatticeInnovations/LatticeOnFhirMain/issues/81

--- app/src/main/java/com/latticeonfhir/android/data/server/model/authentication/TokenResponse.kt
+++ app/src/main/java/com/latticeonfhir/android/data/server/model/authentication/TokenResponse.kt
@@ -1,6 +1,6 @@
 package com.latticeonfhir.android.data.server.model.authentication
 
-import com.google.errorprone.annotations.Keep
+import androidx.annotation.Keep
 
 @Keep
 data class TokenResponse(
