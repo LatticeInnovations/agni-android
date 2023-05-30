f25709f213ebf49dbdb3463015f4d50a1c8beb52
Hâwk <naveen.s1605@gmail.com>
2023-05-30 12:11:27 +0530

    Package Refactor and Parcelize Data class

--- app/src/main/java/com/latticeonfhir/android/data/server/model/patient/PatientIdentifier.kt
+++ app/src/main/java/com/latticeonfhir/android/data/server/model/patient/PatientIdentifier.kt
@@ -1,11 +1,13 @@
 package com.latticeonfhir.android.data.server.model.patient
 
+import android.os.Parcelable
 import androidx.annotation.Keep
-import com.latticeonfhir.android.base.baseclass.ParcelableClass
+import kotlinx.parcelize.Parcelize
 
 @Keep
+@Parcelize
 data class PatientIdentifier(
     val identifierType: String,
     val identifierNumber: String,
     val code: String?
-)
+): Parcelable
