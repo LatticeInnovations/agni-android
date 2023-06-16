3d27de33d9c3ba35b8f562d462e7968118515f53
Hâwk <naveen.s1605@gmail.com>
2023-06-16 16:06:55 +0530

    Added Delay in Periodic Worker to get status every 15 mins

--- app/src/main/java/com/latticeonfhir/android/service/workmanager/workers/upload/patient/post/PatientUploadSyncWorker.kt
+++ app/src/main/java/com/latticeonfhir/android/service/workmanager/workers/upload/patient/post/PatientUploadSyncWorker.kt
@@ -5,6 +5,7 @@ import androidx.work.WorkerParameters
 import androidx.work.workDataOf
 import com.latticeonfhir.android.service.workmanager.workers.base.SyncWorker
 import com.latticeonfhir.android.utils.constants.ErrorConstants
+import com.latticeonfhir.android.utils.constants.ErrorConstants.ERROR_MESSAGE
 import com.latticeonfhir.android.utils.converters.server.responsemapper.ApiContinueResponse
 import com.latticeonfhir.android.utils.converters.server.responsemapper.ApiEmptyResponse
 import com.latticeonfhir.android.utils.converters.server.responsemapper.ApiEndResponse
@@ -15,20 +16,20 @@ import kotlinx.coroutines.delay
 abstract class PatientUploadSyncWorker(context: Context, workerParameters: WorkerParameters): SyncWorker(context,workerParameters) {
 
     override suspend fun doWork(): Result {
-        setProgress(workDataOf(PatientUploadProgress to 0))
-        delay(1000L)
         return when(val response = getSyncRepository().sendPersonPostData()) {
             is ApiContinueResponse -> Result.success()
             is ApiEndResponse -> Result.retry()
             is ApiErrorResponse -> {
-                if (response.errorMessage == ErrorConstants.SESSION_EXPIRED || response.errorMessage == ErrorConstants.UNAUTHORIZED) Result.failure(
-                    workDataOf("errorMsg" to response.errorMessage)
-                )
+                if (response.errorMessage == ErrorConstants.SESSION_EXPIRED || response.errorMessage == ErrorConstants.UNAUTHORIZED) {
+                    setProgress(workDataOf(ERROR_MESSAGE to response.errorMessage))
+                    delay(5000)
+                    Result.failure()
+                }
                 else Result.retry()
             }
             is ApiEmptyResponse -> {
                 setProgress(workDataOf(PatientUploadProgress to 100))
-                delay(1000L)
+                delay(5000)
                 Result.success()
             }
             is ApiNullResponse -> Result.failure()
