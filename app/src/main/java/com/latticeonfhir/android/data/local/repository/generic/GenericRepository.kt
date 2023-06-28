cdb773f5357db85e4818a1199cc61b3e2606351a
Hâwk <naveen.s1605@gmail.com>
2023-06-28 10:37:19 +0530

    Generic Repo 100% Code Coverage

--- app/src/main/java/com/latticeonfhir/android/data/local/repository/generic/GenericRepository.kt
+++ app/src/main/java/com/latticeonfhir/android/data/local/repository/generic/GenericRepository.kt
@@ -2,11 +2,12 @@ package com.latticeonfhir.android.data.local.repository.generic
 
 import com.latticeonfhir.android.data.local.enums.GenericTypeEnum
 import com.latticeonfhir.android.data.local.roomdb.entities.generic.GenericEntity
+import com.latticeonfhir.android.utils.builders.UUIDBuilder
 
 interface GenericRepository {
 
-    suspend fun insertOrUpdatePostEntity(patientId: String, entity: Any, typeEnum: GenericTypeEnum, replaceEntireRow: Boolean = false): Long
-    suspend fun insertOrUpdatePatchEntity(patientFhirId: String,map: Map<String,Any>, typeEnum: GenericTypeEnum): Long
+    suspend fun insertOrUpdatePostEntity(patientId: String, entity: Any, typeEnum: GenericTypeEnum, replaceEntireRow: Boolean = false, uuid: String = UUIDBuilder.generateUUID()): Long
+    suspend fun insertOrUpdatePatchEntity(patientFhirId: String,map: Map<String,Any>, typeEnum: GenericTypeEnum, uuid: String = UUIDBuilder.generateUUID()): Long
 
     suspend fun getNonSyncedPostRelations(): List<GenericEntity>
 }
