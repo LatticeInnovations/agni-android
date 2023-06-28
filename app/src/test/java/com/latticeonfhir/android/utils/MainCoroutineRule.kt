dffccf9a3808108ae839e1493c591735ec18173f
Hâwk <naveen.s1605@gmail.com>
2023-06-28 15:51:13 +0530

    ConnectRelationShip ViewModel Unit Test

--- app/src/test/java/com/latticeonfhir/android/utils/MainCoroutineRule.kt
+++ app/src/test/java/com/latticeonfhir/android/utils/MainCoroutineRule.kt
@@ -1,5 +1,6 @@
 package com.latticeonfhir.android.utils
 
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
@@ -8,19 +9,19 @@ import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
+import kotlin.coroutines.ContinuationInterceptor
 
 @ExperimentalCoroutinesApi
-class MainCoroutineRule(
-    private val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
-) : TestWatcher(), TestCoroutineScope by TestCoroutineScope(dispatcher) {
-    override fun starting(description: Description?) {
+class MainCoroutineRule : TestWatcher(), TestCoroutineScope by TestCoroutineScope() {
+
+    override fun starting(description: Description) {
         super.starting(description)
-        Dispatchers.setMain(dispatcher)
+        Dispatchers.setMain(this.coroutineContext[ContinuationInterceptor] as CoroutineDispatcher)
     }
 
-    override fun finished(description: Description?) {
+    override fun finished(description: Description) {
         super.finished(description)
-        cleanupTestCoroutines()
         Dispatchers.resetMain()
     }
+
 }
