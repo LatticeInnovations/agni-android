90018bfefe77b37065e8c9cc9c81945fb8b12d35
Hâwk <naveen.s1605@gmail.com>
2023-06-28 15:51:13 +0530

    ConnectRelationShip ViewModel Unit Test

--- app/src/test/java/com/latticeonfhir/android/viewmodel/patientregistration/ConfirmRelationshipViewModelTest.kt
+++ app/src/test/java/com/latticeonfhir/android/viewmodel/patientregistration/ConfirmRelationshipViewModelTest.kt
@@ -1,17 +1,29 @@
 package com.latticeonfhir.android.viewmodel.patientregistration
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.latticeonfhir.android.data.local.repository.generic.GenericRepository
 import com.latticeonfhir.android.data.local.repository.patient.PatientRepository
 import com.latticeonfhir.android.data.local.repository.relation.RelationRepository
 import com.latticeonfhir.android.base.BaseClass
+import com.latticeonfhir.android.data.local.enums.RelationEnum
+import com.latticeonfhir.android.data.local.roomdb.dao.RelationDao
 import com.latticeonfhir.android.ui.patientregistration.step4.ConfirmRelationshipViewModel
+import com.latticeonfhir.android.utils.MainCoroutineRule
+import com.latticeonfhir.android.utils.converters.responseconverter.toRelation
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Assert
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
@@ -25,11 +37,21 @@ class ConfirmRelationshipViewModelTest: BaseClass() {
     lateinit var relationRepository: RelationRepository
     @Mock
     lateinit var genericRepository: GenericRepository
+    @Mock
+    private lateinit var relationDao: RelationDao
+
     lateinit var viewModel: ConfirmRelationshipViewModel
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
     @Before
     public override fun setUp(){
-        MockitoAnnotations.initMocks(this)
+        MockitoAnnotations.openMocks(this)
         viewModel = ConfirmRelationshipViewModel(relationRepository, patientRepository, genericRepository)
     }
 
@@ -37,7 +59,7 @@ class ConfirmRelationshipViewModelTest: BaseClass() {
     fun getPatientDataTest() = runBlocking {
         `when`(patientRepository.getPatientById(viewModel.patientId)).thenReturn(listOf(patientResponse))
 
-        viewModel.getPatientData(viewModel.patientId){
+        viewModel.getPatientData(viewModel.patientId) {
             Assert.assertEquals(patientResponse, it)
         }
     }
@@ -47,9 +69,9 @@ class ConfirmRelationshipViewModelTest: BaseClass() {
         `when`(relationRepository.getRelationBetween(viewModel.patientId, viewModel.relativeId)).thenReturn(
             listOf(relationView)
         )
-        viewModel.getRelationBetween(viewModel.patientId, viewModel.relativeId)
+        val result = viewModel.getRelationBetween(viewModel.patientId, viewModel.relativeId)
         delay(5000)
-        Assert.assertEquals(listOf(relationView), viewModel.relationBetween)
+        Assert.assertEquals(Unit, result)
     }
 
     @Test
@@ -57,6 +79,28 @@ class ConfirmRelationshipViewModelTest: BaseClass() {
         `when`(relationRepository.deleteRelation(viewModel.patientId, viewModel.relativeId)).thenReturn(1)
         val result = viewModel.deleteRelation(viewModel.patientId, viewModel.relativeId)
         delay(5000)
-        Assert.assertEquals(listOf(relationView), viewModel.relationBetween)
+        Assert.assertEquals(Unit,result)
+    }
+
+    @Test
+    fun `discard relations`() = runTest{
+        val result = viewModel.discardRelations()
+        delay(5000)
+        assertEquals(Unit,result)
+    }
+
+    @Test
+    fun `update relations`() = runTest{
+        val result = viewModel.updateRelation(relationEntity.toRelation())
+        delay(5000)
+        assertEquals(Unit,result)
+    }
+
+    @Test
+    fun `add relation to genericEntity`() = runTest {
+        viewModel.relationBetween = listOf(relationView)
+        val result = viewModel.addRelationsToGenericEntity()
+        delay(5000)
+        assertEquals(Unit,result)
     }
 }
