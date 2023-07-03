30714b8f2b6273d31cdd13397ee405bd2efd812b
Hâwk <naveen.s1605@gmail.com>
2023-07-03 15:47:00 +0530

    https://github.com/LatticeInnovations/LatticeOnFhirMain/issues/145

--- app/src/main/java/com/latticeonfhir/android/data/local/sharedpreferences/PreferenceStorageImpl.kt
+++ app/src/main/java/com/latticeonfhir/android/data/local/sharedpreferences/PreferenceStorageImpl.kt
@@ -10,10 +10,14 @@ import java.util.Date
 class PreferenceStorageImpl(private val sharedPreferences: SharedPreferences) : PreferenceStorage {
 
     override var token by StringPreference(sharedPreferences, PREF_TOKEN,"")
+    override var userFhirId by StringPreference(sharedPreferences, PREF_USER_FHIR_ID,"")
     override var userName by StringPreference(sharedPreferences, PREF_USER_NAME,"")
-    override var userRole by StringPreference(sharedPreferences, PREF_USER_ROLE,"")
     override var userMobile by LongPreference(sharedPreferences, PREF_USER_MOBILE,0L)
     override var userEmail by StringPreference(sharedPreferences, PREF_USER_EMAIL,"")
+    override var userRoleId by StringPreference(sharedPreferences, PREF_USER_ROLE_ID,"")
+    override var userRole by StringPreference(sharedPreferences, PREF_USER_ROLE,"")
+    override val organizationFhirId by StringPreference(sharedPreferences, PREF_ORGANIZATION_FHIR_ID,"")
+    override val organization by StringPreference(sharedPreferences, PREF_ORGANIZATION,"")
     override var roomDBEncryptionKey by StringPreference(sharedPreferences, PREF_ROOM_ENCRYPTION_KEY,"")
     override var lastPatientSyncTime by LongPreference(sharedPreferences, PREF_LAST_PATIENT_SYNC_TIME,0L)
     override var lastRelationSyncTime by LongPreference(sharedPreferences, PREF_LAST_RELATION_SYNC_TIME,0L)
@@ -33,10 +37,14 @@ class PreferenceStorageImpl(private val sharedPreferences: SharedPreferences) : 
 
         const val PREF_TOKEN = "pref_token"
 
+        const val PREF_USER_FHIR_ID = "pref_user_fhir_id"
         const val PREF_USER_NAME = "pref_user_name"
-        const val PREF_USER_ROLE = "pref_user_role"
         const val PREF_USER_MOBILE = "pref_user_mobile"
         const val PREF_USER_EMAIL = "pref_user_email"
+        const val PREF_USER_ROLE_ID = "pref_user_role_id"
+        const val PREF_USER_ROLE = "pref_user_role"
+        const val PREF_ORGANIZATION_FHIR_ID = "pref_organization_fhir_id"
+        const val PREF_ORGANIZATION = "pref_organization"
 
         const val PREF_ROOM_ENCRYPTION_KEY = "pref_room_encryption_key"
 
