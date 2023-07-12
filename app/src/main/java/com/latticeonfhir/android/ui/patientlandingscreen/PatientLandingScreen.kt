df1605de0cae7a8e999ca4d34483288c77e83d41
mansikalraa <mansi.kalra23@gmail.com>
2023-07-12 16:36:47 +0530

    patient landing screen tab update

--- app/src/main/java/com/latticeonfhir/android/ui/patientlandingscreen/PatientLandingScreen.kt
+++ app/src/main/java/com/latticeonfhir/android/ui/patientlandingscreen/PatientLandingScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -75,7 +76,9 @@ fun PatientLandingScreen(
                                 viewModel.patient?.lastName
                             ),
                             style = MaterialTheme.typography.titleLarge,
-                            modifier = Modifier.testTag("TITLE")
+                            modifier = Modifier.testTag("TITLE"),
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
                         )
                         Text(
                             text = subTitle,
