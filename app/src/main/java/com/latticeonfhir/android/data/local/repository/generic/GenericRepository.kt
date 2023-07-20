34df803bba967c59b8d20eb27be509c9d8d90903
Hâwk <naveen.s1605@gmail.com>
2023-07-20 12:14:03 +0530

    Pass UUID in parameter for Unit Testing

--- app/src/main/java/com/latticeonfhir/android/data/local/repository/generic/GenericRepository.kt
+++ app/src/main/java/com/latticeonfhir/android/data/local/repository/generic/GenericRepository.kt
@@ -1,20 +1,26 @@
 package com.latticeonfhir.android.data.local.repository.generic
 
 import com.latticeonfhir.android.data.local.enums.GenericTypeEnum
-import com.latticeonfhir.android.data.local.roomdb.entities.generic.GenericEntity
 import com.latticeonfhir.android.data.server.model.patient.PatientResponse
 import com.latticeonfhir.android.data.server.model.prescription.prescriptionresponse.PrescriptionResponse
 import com.latticeonfhir.android.data.server.model.relatedperson.RelatedPersonResponse
 import com.latticeonfhir.android.utils.builders.UUIDBuilder
 
+/**
+ *
+ * Here we are passing UUID in Parameters due to Unit Testing Scenario.
+ * if we generate UUID in repo Unit tests were failing.
+ * Do not pass uuid from anywhere else it will automatically generate here.
+ *
+ */
 interface GenericRepository {
 
-    suspend fun insertPatient(patientResponse: PatientResponse): Long
+    suspend fun insertPatient(patientResponse: PatientResponse, uuid: String = UUIDBuilder.generateUUID()): Long
 
-    suspend fun insertRelation(patientId: String, relatedPersonResponse: RelatedPersonResponse): Long
+    suspend fun insertRelation(patientId: String, relatedPersonResponse: RelatedPersonResponse, uuid: String = UUIDBuilder.generateUUID()): Long
     suspend fun updateRelationFhirId()
 
-    suspend fun insertPrescription(prescriptionResponse: PrescriptionResponse): Long
+    suspend fun insertPrescription(prescriptionResponse: PrescriptionResponse, uuid: String = UUIDBuilder.generateUUID()): Long
     suspend fun updatePrescriptionFhirId()
 
     @Deprecated("This method was deprecated use above methods to store POST Generic Entity")
