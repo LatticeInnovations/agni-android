7f8917f5b18f745d97806f7fe244b3c9edf1e368
mansikalraa <mansi.kalra23@gmail.com>
2023-07-28 17:36:21 +0530

    appointment status update at midnight worker

--- app/src/main/java/com/latticeonfhir/android/data/local/enums/AppointmentStatusEnum.kt
+++ app/src/main/java/com/latticeonfhir/android/data/local/enums/AppointmentStatusEnum.kt
@@ -1,11 +1,11 @@
 package com.latticeonfhir.android.data.local.enums
 
 enum class AppointmentStatusEnum(val value: String) {
-    Arrived("arrived"),
-    WalkIn("walkin"),
-    Scheduled("scheduled"),
-    Cancelled("cancelled"),
-    InProgress("In-progress"),
-    Completed("completed"),
-    NoShow("noshow")
+    ARRIVED("arrived"),
+    WALK_IN("walkin"),
+    SCHEDULED("scheduled"),
+    CANCELLED("cancelled"),
+    IN_PROGRESS("In-progress"),
+    COMPLETED("completed"),
+    NO_SHOW("noshow")
 }
