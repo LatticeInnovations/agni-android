aee7a9b592884aede34fb68860c15d684b6a1c2f
mansikalraa <mansi.kalra23@gmail.com>
2023-07-28 13:47:13 +0530

    server response, entities, and dao for appointment and schedule

---
+++ app/src/main/java/com/latticeonfhir/android/data/local/roomdb/dao/AppointmentDao.kt
@@ -0,0 +1,37 @@
+package com.latticeonfhir.android.data.local.roomdb.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Update
+import com.latticeonfhir.android.data.local.roomdb.entities.appointment.AppointmentEntity
+import java.util.Date
+
+@Dao
+interface AppointmentDao {
+    @Transaction
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAppointmentEntity(vararg appointmentEntity: AppointmentEntity): List<Long>
+
+    @Transaction
+    @Query("UPDATE AppointmentEntity SET appointmentFhirId=:fhirId WHERE id=:id")
+    suspend fun updateAppointmentFhirId(id: String, fhirId: String): Int
+
+    @Transaction
+    @Query("SELECT * FROM AppointmentEntity WHERE patientId=:patientId and status=:status")
+    suspend fun getAppointmentsOfPatientByStatus(patientId: String, status: String): List<AppointmentEntity>
+
+    @Transaction
+    @Query("SELECT * FROM AppointmentEntity WHERE startTime BETWEEN :startOfDay AND :endOfDay")
+    suspend fun getAppointmentsByDate(startOfDay: Date, endOfDay: Date): List<AppointmentEntity>
+
+    @Transaction
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateAppointmentEntity(appointmentEntity: AppointmentEntity): Int
+
+    @Transaction
+    @Query("UPDATE AppointmentEntity SET status=:toStatus WHERE status=:fromStatus and endTime<:endOfDay")
+    suspend fun updateAppointmentStatus(toStatus: String, fromStatus: String, endOfDay: Date): Int
+}
---
+++ app/src/main/java/com/latticeonfhir/android/data/server/model/scheduleandappointment/schedule/ScheduleResponse.kt
@@ -0,0 +1,16 @@
+package com.latticeonfhir.android.data.server.model.scheduleandappointment.schedule
+
+import android.os.Parcelable
+import androidx.annotation.Keep
+import com.latticeonfhir.android.data.server.model.scheduleandappointment.Slot
+import kotlinx.parcelize.Parcelize
+
+@Keep
+@Parcelize
+data class ScheduleResponse(
+    val uuid: String,
+    val scheduleId: String,
+    val planningHorizon: Slot,
+    val orgId: String,
+    val bookedSlots: Int
+): Parcelable
