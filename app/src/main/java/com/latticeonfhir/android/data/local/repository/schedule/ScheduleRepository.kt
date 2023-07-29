783a3897b824de6b7e237c6f4f64a864f3c09944
mansikalraa <mansi.kalra23@gmail.com>
2023-07-29 23:03:37 +0530

    method to update schedule entity

--- app/src/main/java/com/latticeonfhir/android/data/local/repository/schedule/ScheduleRepository.kt
+++ app/src/main/java/com/latticeonfhir/android/data/local/repository/schedule/ScheduleRepository.kt
@@ -1,7 +1,9 @@
 package com.latticeonfhir.android.data.local.repository.schedule
 
+import com.latticeonfhir.android.data.server.model.scheduleandappointment.schedule.ScheduleResponse
 import java.util.Date
 
 interface ScheduleRepository {
     suspend fun getBookedSlotsCount(startTime: Date): Int
+    suspend fun updateSchedule(scheduleResponse: ScheduleResponse): Int
 }
