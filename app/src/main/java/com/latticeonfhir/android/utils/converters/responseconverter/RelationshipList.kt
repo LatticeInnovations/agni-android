1e183c6518a9a44d2d8ff9e94223e35dcfb828a3
Hâwk <naveen.s1605@gmail.com>
2023-09-07 13:52:52 +0530

    Sonar Lint Fixes Relationship Converter

--- app/src/main/java/com/latticeonfhir/android/utils/converters/responseconverter/RelationshipList.kt
+++ app/src/main/java/com/latticeonfhir/android/utils/converters/responseconverter/RelationshipList.kt
@@ -2,40 +2,42 @@ package com.latticeonfhir.android.utils.converters.responseconverter
 
 object RelationshipList {
     internal fun getRelationshipList(gender: String): List<String> {
-        if (gender == "male") return listOf(
-            "Son",
-            "Father",
-            "Grand Father",
-            "Brother",
-            "Grand Son",
-            "Uncle",
-            "Brother-in-law",
-            "Father-in-law",
-            "Son-in-law",
-            "Nephew",
-            "Husband"
-        )
-        else if (gender == "female") return listOf(
-            "Daughter",
-            "Mother",
-            "Grand Mother",
-            "Sister",
-            "Grand Daughter",
-            "Aunty",
-            "Sister-in-law",
-            "Mother-in-law",
-            "Daughter-in-law",
-            "Niece",
-            "Wife"
-        )
-        else return listOf(
-            "Child",
-            "Parent",
-            "Grand Parent",
-            "Sibling",
-            "Grand Child",
-            "In-Law",
-            "Spouse"
-        )
+        return when (gender) {
+            "male" -> listOf(
+                "Son",
+                "Father",
+                "Grand Father",
+                "Brother",
+                "Grand Son",
+                "Uncle",
+                "Brother-in-law",
+                "Father-in-law",
+                "Son-in-law",
+                "Nephew",
+                "Husband"
+            )
+            "female" -> listOf(
+                "Daughter",
+                "Mother",
+                "Grand Mother",
+                "Sister",
+                "Grand Daughter",
+                "Aunty",
+                "Sister-in-law",
+                "Mother-in-law",
+                "Daughter-in-law",
+                "Niece",
+                "Wife"
+            )
+            else -> listOf(
+                "Child",
+                "Parent",
+                "Grand Parent",
+                "Sibling",
+                "Grand Child",
+                "In-Law",
+                "Spouse"
+            )
+        }
     }
 }
