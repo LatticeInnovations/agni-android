29beec9c3d3e716faa4cb0a56bbbb8d5ce5bfe36
mansikalraa <mansi.kalra23@gmail.com>
2024-03-08 15:55:18 +0530

    patient landing screen fhir integration

--- app/src/main/java/com/latticeonfhir/android/ui/patientlandingscreen/PatientLandingScreen.kt
+++ app/src/main/java/com/latticeonfhir/android/ui/patientlandingscreen/PatientLandingScreen.kt
@@ -2,12 +2,31 @@ package com.latticeonfhir.android.ui.patientlandingscreen
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.KeyboardArrowRight
-import androidx.compose.material3.*
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
@@ -18,15 +37,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.*
 import androidx.navigation.NavController
 import com.latticeonfhir.android.R
-import com.latticeonfhir.android.data.server.model.patient.PatientResponse
 import com.latticeonfhir.android.navigation.Screen
-import com.latticeonfhir.android.ui.common.appointmentsfab.AppointmentsFab
-import com.latticeonfhir.android.utils.converters.responseconverter.NameConverter
+import com.latticeonfhir.android.utils.constants.NavControllerConstants.PATIENT
 import com.latticeonfhir.android.utils.converters.responseconverter.TimeConverter.toAge
-import com.latticeonfhir.android.utils.converters.responseconverter.TimeConverter.toTimeInMilli
+import org.hl7.fhir.r4.model.Patient
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -37,143 +53,131 @@ fun PatientLandingScreen(
     LaunchedEffect(viewModel.isLaunched) {
         if (!viewModel.isLaunched) {
             viewModel.patient =
-                navController.previousBackStackEntry?.savedStateHandle?.get<PatientResponse>(
-                    "patient"
-                )
-            viewModel.patient?.fhirId?.let { patientFhirId ->
-                viewModel.downloadPrescriptions(
-                    patientFhirId
-                )
-            }
+                navController.previousBackStackEntry?.savedStateHandle?.get<Patient>(
+                    PATIENT
+                )!!
         }
-        viewModel.patient = viewModel.getPatientData(viewModel.patient!!.id)
-
+        viewModel.patient = viewModel.getPatientData()
+        viewModel.getScheduledAppointmentsCount()
         viewModel.isLaunched = true
-    }
-    LaunchedEffect(true) {
-        viewModel.patient?.id?.let { id ->
-            viewModel.getScheduledAppointmentsCount(id)
-        }
     }
     BackHandler(enabled = true) {
         if (viewModel.isFabSelected) viewModel.isFabSelected = false
         else navController.popBackStack()
     }
-    Scaffold(
-        topBar = {
-            LargeTopAppBar(
-                modifier = Modifier.fillMaxWidth(),
-                colors = TopAppBarDefaults.largeTopAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(8.dp)
-                ),
-                navigationIcon = {
-                    IconButton(onClick = {
-                        navController.popBackStack()
-                    }) {
-                        Icon(Icons.Default.ArrowBack, contentDescription = "BACK_ICON")
+    if (viewModel.isLaunched) {
+        Scaffold(
+            topBar = {
+                LargeTopAppBar(
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = TopAppBarDefaults.largeTopAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(8.dp)
+                    ),
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            navController.popBackStack()
+                        }) {
+                            Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "BACK_ICON")
+                        }
+                    },
+                    title = {
+                        val age = viewModel.patient.birthDate.time.toAge()
+                        val subTitle = "${
+                            viewModel.patient.gender.display
+                        }/$age · +91 ${viewModel.patient.telecom[0].value}"
+                        Column {
+                            Text(
+                                text = viewModel.patient.nameFirstRep.nameAsSingleString,
+                                style = MaterialTheme.typography.titleLarge,
+                                modifier = Modifier.testTag("TITLE"),
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis
+                            )
+                            Text(
+                                text = subTitle,
+                                style = MaterialTheme.typography.bodyLarge,
+                                modifier = Modifier.testTag("SUBTITLE")
+                            )
+                        }
+                    },
+                    actions = {
+                        IconButton(onClick = {
+                            navController.currentBackStackEntry?.savedStateHandle?.set(
+                                key = "patient_detailsID",
+                                value = viewModel.patient.id
+                            )
+                            navController.navigate(Screen.PatientProfile.route)
+                        }) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.profile_icon),
+                                contentDescription = "profile icon",
+                                tint = MaterialTheme.colorScheme.primary,
+                                modifier = Modifier.size(20.dp)
+                            )
+                        }
                     }
-                },
-                title = {
-                    val age = viewModel.patient?.birthDate?.toTimeInMilli()?.toAge()
-                    val subTitle = "${
-                        viewModel.patient?.gender?.get(0)?.uppercase()
-                    }/$age · +91 ${viewModel.patient?.mobileNumber} ${if (viewModel.patient?.fhirId.isNullOrEmpty()) "" else " · ${viewModel.patient?.fhirId}"} "
-                    Column {
-                        Text(
-                            text = NameConverter.getFullName(
-                                viewModel.patient?.firstName,
-                                viewModel.patient?.middleName,
-                                viewModel.patient?.lastName
-                            ),
-                            style = MaterialTheme.typography.titleLarge,
-                            modifier = Modifier.testTag("TITLE"),
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis
+                )
+            },
+            content = {
+                Box(modifier = Modifier.padding(it)) {
+                    Column(modifier = Modifier.padding(horizontal = 20.dp, vertical = 30.dp)) {
+                        CardComposable(
+                            navController,
+                            viewModel.patient,
+                            viewModel,
+                            "APPOINTMENTS",
+                            Screen.Appointments.route,
+                            stringResource(id = R.string.appointments),
+                            R.drawable.event_note_icon
                         )
-                        Text(
-                            text = subTitle,
-                            style = MaterialTheme.typography.bodyLarge,
-                            modifier = Modifier.testTag("SUBTITLE")
+                        CardComposable(
+                            navController,
+                            viewModel.patient,
+                            viewModel,
+                            "HOUSEHOLD_MEMBER",
+                            Screen.HouseholdMembersScreen.route,
+                            stringResource(id = R.string.household_members),
+                            R.drawable.group_icon
                         )
-                    }
-                },
-                actions = {
-                    IconButton(onClick = {
-
-                        navController.currentBackStackEntry?.savedStateHandle?.set(
-                            key = "patient_detailsID",
-                            value = viewModel.patient?.id
-                        )
-                        navController.navigate(Screen.PatientProfile.route)
-                    }) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.profile_icon),
-                            contentDescription = "profile icon",
-                            tint = MaterialTheme.colorScheme.primary,
-                            modifier = Modifier.size(20.dp)
+                        CardComposable(
+                            navController,
+                            viewModel.patient,
+                            viewModel,
+                            "PRESCRIPTION",
+                            Screen.Prescription.route,
+                            stringResource(id = R.string.prescription),
+                            R.drawable.prescriptions_icon
                         )
                     }
-                }
-            )
-        },
-        content = {
-            Box(modifier = Modifier.padding(it)) {
-                Column(modifier = Modifier.padding(horizontal = 20.dp, vertical = 30.dp)) {
-                    CardComposable(
-                        navController,
-                        viewModel.patient,
-                        viewModel,
-                        "APPOINTMENTS",
-                        Screen.Appointments.route,
-                        stringResource(id = R.string.appointments),
-                        R.drawable.event_note_icon
-                    )
-                    CardComposable(
-                        navController,
-                        viewModel.patient,
-                        viewModel,
-                        "HOUSEHOLD_MEMBER",
-                        Screen.HouseholdMembersScreen.route,
-                        stringResource(id = R.string.household_members),
-                        R.drawable.group_icon
-                    )
-                    CardComposable(
-                        navController,
-                        viewModel.patient,
-                        viewModel,
-                        "PRESCRIPTION",
-                        Screen.Prescription.route,
-                        stringResource(id = R.string.prescription),
-                        R.drawable.prescriptions_icon
-                    )
-                }
-                if (viewModel.showAllSlotsBookedDialog) {
-                    AllSlotsBookedDialog {
-                        viewModel.showAllSlotsBookedDialog = false
+                    if (viewModel.showAllSlotsBookedDialog) {
+                        AllSlotsBookedDialog {
+                            viewModel.showAllSlotsBookedDialog = false
+                        }
                     }
                 }
+            },
+            floatingActionButton = {
+                // TODO: to be implemented after appointment screen binding
+//            viewModel.patient?.let { patient ->
+//                AppointmentsFab(
+//                    navController,
+//                    patient,
+//                    viewModel.isFabSelected
+//                ) { showDialog ->
+//                    if (showDialog) {
+//                        viewModel.showAllSlotsBookedDialog = true
+//                    } else viewModel.isFabSelected = !viewModel.isFabSelected
+//                }
+//            }
             }
-        },
-        floatingActionButton = {
-            viewModel.patient?.let { patient ->
-                AppointmentsFab(
-                    navController,
-                    patient,
-                    viewModel.isFabSelected
-                ) { showDialog ->
-                    if (showDialog) {
-                        viewModel.showAllSlotsBookedDialog = true
-                    } else viewModel.isFabSelected = !viewModel.isFabSelected
-                }
-            }
-        }
-    )
+        )
+    }
 }
 
 @Composable
 fun CardComposable(
     navController: NavController,
-    patient: PatientResponse?,
+    patient: Patient,
     viewModel: PatientLandingScreenViewModel,
     tag: String,
     route: String,
@@ -187,7 +191,7 @@ fun CardComposable(
             .padding(bottom = 20.dp)
             .clickable {
                 navController.currentBackStackEntry?.savedStateHandle?.set(
-                    "patient",
+                    PATIENT,
                     patient
                 )
                 navController.navigate(route)
@@ -216,7 +220,7 @@ fun CardComposable(
                     if (label == stringResource(id = R.string.appointments)) Text(
                         text = stringResource(
                             id = R.string.appointments_scheduled,
-                            viewModel.appointmentsCount
+                            viewModel.appointmentsIds.size
                         ),
                         style = MaterialTheme.typography.bodyMedium,
                         color = MaterialTheme.colorScheme.onSurfaceVariant,
@@ -225,7 +229,7 @@ fun CardComposable(
                 }
             }
             Spacer(modifier = Modifier.weight(1f))
-            Icon(Icons.Default.KeyboardArrowRight, contentDescription = "RIGHT_ARROW")
+            Icon(Icons.AutoMirrored.Filled.KeyboardArrowRight, contentDescription = "RIGHT_ARROW")
         }
     }
 }
