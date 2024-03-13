38091e98f70f4f36bb1d366d81cd2a48eb464865
mansikalraa <mansi.kalra23@gmail.com>
2024-03-13 13:45:18 +0530

    get related person to common query

--- app/src/main/java/com/latticeonfhir/android/ui/householdmember/members/MembersScreenViewModel.kt
+++ app/src/main/java/com/latticeonfhir/android/ui/householdmember/members/MembersScreenViewModel.kt
@@ -6,15 +6,13 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewModelScope
 import com.google.android.fhir.FhirEngine
 import com.google.android.fhir.SearchResult
-import com.google.android.fhir.search.include
-import com.google.android.fhir.search.search
 import com.latticeonfhir.android.base.viewmodel.BaseViewModel
 import com.latticeonfhir.android.utils.fhirengine.FhirQueries.getPersonResource
+import com.latticeonfhir.android.utils.fhirengine.FhirQueries.getRelatedPerson
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.hl7.fhir.r4.model.RelatedPerson
-import org.hl7.fhir.r4.model.ResourceType
 import javax.inject.Inject
 
 @HiltViewModel
@@ -29,14 +27,10 @@ class MembersScreenViewModel @Inject constructor(
             val person = getPersonResource(fhirEngine, patientId)
             person.link.forEach { relatedPersonLink ->
                 if (relatedPersonLink.target.reference.contains("RelatedPerson")) {
-                    fhirEngine.search<RelatedPerson> {
-                        filter(
-                            RelatedPerson.RES_ID, {
-                                value = of(relatedPersonLink.target.reference.substringAfter("/"))
-                            }
-                        )
-                        include(ResourceType.Patient, RelatedPerson.PATIENT)
-                    }.forEach { result ->
+                    getRelatedPerson(
+                        fhirEngine,
+                        relatedPersonLink.target.reference.substringAfter("/")
+                    ).forEach { result ->
                         relationsListWithRelation = relationsListWithRelation + listOf(result)
                     }
                 }
