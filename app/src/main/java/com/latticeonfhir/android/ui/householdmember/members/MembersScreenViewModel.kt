54e7f0b30b7de76f0db17512a338b4b4347f857b
mansikalraa <mansi.kalra23@gmail.com>
2024-03-13 12:44:23 +0530

    household member members screen fhir resource binding

--- app/src/main/java/com/latticeonfhir/android/ui/householdmember/members/MembersScreenViewModel.kt
+++ app/src/main/java/com/latticeonfhir/android/ui/householdmember/members/MembersScreenViewModel.kt
@@ -4,54 +4,44 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewModelScope
+import com.google.android.fhir.FhirEngine
+import com.google.android.fhir.SearchResult
+import com.google.android.fhir.search.include
+import com.google.android.fhir.search.search
 import com.latticeonfhir.android.base.viewmodel.BaseViewModel
-import com.latticeonfhir.android.data.local.model.patient.PatientResponseWithRelation
-import com.latticeonfhir.android.data.local.repository.patient.PatientRepository
-import com.latticeonfhir.android.data.local.repository.relation.RelationRepository
-import com.latticeonfhir.android.data.local.roomdb.entities.relation.RelationEntity
-import com.latticeonfhir.android.data.server.model.patient.PatientResponse
+import com.latticeonfhir.android.utils.fhirengine.FhirQueries.getPersonResource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import timber.log.Timber
+import org.hl7.fhir.r4.model.RelatedPerson
+import org.hl7.fhir.r4.model.ResourceType
 import javax.inject.Inject
 
 @HiltViewModel
 class MembersScreenViewModel @Inject constructor(
-    private val relationRepository: RelationRepository,
-    private val patientRepository: PatientRepository
+    private val fhirEngine: FhirEngine
 ) : BaseViewModel() {
     var loading by mutableStateOf(true)
-    var relationsList by mutableStateOf(listOf<RelationEntity>())
-    var relationsListWithRelation by mutableStateOf(listOf<PatientResponseWithRelation>())
+    var relationsListWithRelation by mutableStateOf(listOf<SearchResult<RelatedPerson>>())
 
     internal fun getAllRelations(patientId: String) {
         viewModelScope.launch(Dispatchers.IO) {
-            relationsList = relationRepository.getAllRelationOfPatient(patientId)
-            relationsList.forEach { relation ->
-                val patientResponseWithRelation = getPatientData(relation.toId)
-                if (!relationsListWithRelation.contains(
-                        PatientResponseWithRelation(
-                            patientResponseWithRelation,
-                            relation.relation
+            val person = getPersonResource(fhirEngine, patientId)
+            person.link.forEach { relatedPersonLink ->
+                if (relatedPersonLink.target.reference.contains("RelatedPerson")) {
+                    fhirEngine.search<RelatedPerson> {
+                        filter(
+                            RelatedPerson.RES_ID, {
+                                value = of(relatedPersonLink.target.reference.substringAfter("/"))
+                            }
                         )
-                    )
-                ) {
-                    relationsListWithRelation = relationsListWithRelation + listOf(
-                        PatientResponseWithRelation(
-                            patientResponseWithRelation,
-                            relation.relation
-                        )
-                    )
+                        include(ResourceType.Patient, RelatedPerson.PATIENT)
+                    }.forEach { result ->
+                        relationsListWithRelation = relationsListWithRelation + listOf(result)
+                    }
                 }
-
             }
             loading = false
-            Timber.d("manseeyy ${relationsListWithRelation.size} $relationsListWithRelation")
         }
-    }
-
-    internal suspend fun getPatientData(id: String): PatientResponse {
-        return patientRepository.getPatientById(id)[0]
     }
 }
