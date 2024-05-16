9eef6780fc2cf12761054c2711544fa4e58d84e5
mansikalraa <mansi.kalra23@gmail.com>
2024-05-16 11:53:32 +0530

    last updated entity, data layer

---
+++ app/src/main/java/com/latticeonfhir/android/data/local/roomdb/dao/PatientLastUpdatedDao.kt
@@ -0,0 +1,15 @@
+package com.latticeonfhir.android.data.local.roomdb.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Transaction
+import com.latticeonfhir.android.data.local.roomdb.entities.patient.PatientLastUpdatedEntity
+
+@Dao
+interface PatientLastUpdatedDao {
+
+    @Transaction
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPatientLastUpdatedData(patientLastUpdatedEntity: PatientLastUpdatedEntity): Long
+}
