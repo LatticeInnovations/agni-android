9d550428f5bda57d7421921eeb587251f26fd474
mansikalraa <mansi.kalra23@gmail.com>
2024-06-10 15:31:13 +0530

    add to queue dialog in prescription screen

--- app/src/main/java/com/latticeonfhir/android/ui/prescription/photo/view/PrescriptionPhotoViewViewModel.kt
+++ app/src/main/java/com/latticeonfhir/android/ui/prescription/photo/view/PrescriptionPhotoViewViewModel.kt
@@ -5,11 +5,18 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.latticeonfhir.android.data.local.enums.AppointmentStatusEnum
+import com.latticeonfhir.android.data.local.model.appointment.AppointmentResponseLocal
+import com.latticeonfhir.android.data.local.repository.appointment.AppointmentRepository
 import com.latticeonfhir.android.data.local.repository.generic.GenericRepository
+import com.latticeonfhir.android.data.local.repository.patient.lastupdated.PatientLastUpdatedRepository
+import com.latticeonfhir.android.data.local.repository.preference.PreferenceRepository
 import com.latticeonfhir.android.data.local.repository.prescription.PrescriptionRepository
+import com.latticeonfhir.android.data.local.repository.schedule.ScheduleRepository
 import com.latticeonfhir.android.data.local.roomdb.entities.prescription.photo.PrescriptionPhotoEntity
 import com.latticeonfhir.android.data.server.model.patient.PatientResponse
 import com.latticeonfhir.android.data.server.model.prescription.photo.File
+import com.latticeonfhir.android.utils.common.Queries
 import com.latticeonfhir.android.utils.converters.responseconverter.TimeConverter.toEndOfDay
 import com.latticeonfhir.android.utils.converters.responseconverter.TimeConverter.toTodayStartDate
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -21,7 +28,11 @@ import javax.inject.Inject
 @HiltViewModel
 class PrescriptionPhotoViewViewModel @Inject constructor(
     private val prescriptionRepository: PrescriptionRepository,
-    private val genericRepository: GenericRepository
+    private val genericRepository: GenericRepository,
+    private val appointmentRepository: AppointmentRepository,
+    private val scheduleRepository: ScheduleRepository,
+    private val patientLastUpdatedRepository: PatientLastUpdatedRepository,
+    private val preferenceRepository: PreferenceRepository
 ) : ViewModel() {
     var isLaunched by mutableStateOf(false)
     var patient by mutableStateOf<PatientResponse?>(null)
@@ -34,12 +45,41 @@ class PrescriptionPhotoViewViewModel @Inject constructor(
     var displayNote by mutableStateOf(false)
     var prescriptionPhotos by mutableStateOf(listOf<File>())
     var deletedPhotos = mutableListOf<File>()
+    var canAddPrescription by mutableStateOf(false)
+    var showAddToQueueDialog by mutableStateOf(false)
+    var ifAllSlotsBooked by mutableStateOf(false)
+    var isAppointmentCompleted by mutableStateOf(false)
+    var showAppointmentCompletedDialog by mutableStateOf(false)
+    private val maxNumberOfAppointmentsInADay = 250
+    var appointment by mutableStateOf<AppointmentResponseLocal?>(null)
 
     var selectedFile: File? by mutableStateOf(null)
 
     internal fun getPastPrescription() {
         viewModelScope.launch(Dispatchers.IO) {
             prescriptionPhotos = prescriptionRepository.getLastPhotoPrescription(patient!!.id)
+            appointment = appointmentRepository.getAppointmentsOfPatientByStatus(
+                patient!!.id,
+                AppointmentStatusEnum.SCHEDULED.value
+            ).firstOrNull { appointmentResponse ->
+                appointmentResponse.slot.start.time < Date().toEndOfDay() && appointmentResponse.slot.start.time > Date().toTodayStartDate()
+            }
+            appointmentRepository.getAppointmentsOfPatientByDate(
+                patient!!.id,
+                Date().toTodayStartDate(),
+                Date().toEndOfDay()
+            ).let { appointmentResponse ->
+                canAddPrescription =
+                    appointmentResponse?.status == AppointmentStatusEnum.ARRIVED.value || appointmentResponse?.status == AppointmentStatusEnum.WALK_IN.value
+                            || appointmentResponse?.status == AppointmentStatusEnum.IN_PROGRESS.value
+                isAppointmentCompleted = appointmentResponse?.status == AppointmentStatusEnum.COMPLETED.value
+            }
+            ifAllSlotsBooked = appointmentRepository.getAppointmentListByDate(
+                Date().toTodayStartDate(),
+                Date().toEndOfDay()
+            ).filter { appointmentResponseLocal ->
+                appointmentResponseLocal.status != AppointmentStatusEnum.CANCELLED.value
+            }.size >= maxNumberOfAppointmentsInADay
         }
     }
 
@@ -115,6 +155,30 @@ class PrescriptionPhotoViewViewModel @Inject constructor(
             }
             deletedPhotos.add(selectedFile!!)
             deleted()
+        }
+    }
+
+    internal fun addPatientToQueue(patient: PatientResponse, addedToQueue: (List<Long>) -> Unit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            Queries.addPatientToQueue(
+                patient,
+                scheduleRepository,
+                genericRepository,
+                preferenceRepository,
+                appointmentRepository,
+                patientLastUpdatedRepository,
+                addedToQueue
+            )
+        }
+    }
+
+    internal fun updateStatusToArrived(
+        patient: PatientResponse,
+        appointment: AppointmentResponseLocal,
+        updated: (Int) -> Unit
+    ) {
+        viewModelScope.launch(Dispatchers.IO) {
+            Queries.updateStatusToArrived(patient, appointment, appointmentRepository, genericRepository, preferenceRepository, scheduleRepository, patientLastUpdatedRepository, updated)
         }
     }
 }
