2e40b8f296ac75d8b6942eb1f12016466f9e5811
mansikalraa <mansi.kalra23@gmail.com>
2024-06-11 15:23:52 +0530

    syncing enhancement

--- app/src/main/java/com/latticeonfhir/android/service/sync/SyncService.kt
+++ app/src/main/java/com/latticeonfhir/android/service/sync/SyncService.kt
@@ -304,7 +304,7 @@ class SyncService(
             logout
         )?.apply {
             if (this is ApiEmptyResponse || this is ApiEndResponse) {
-                downloadPrescriptionPhoto()
+                downloadPrescriptionPhoto(logout)
             }
         }
     }
@@ -327,8 +327,8 @@ class SyncService(
     }
 
     /** Download Patient Last Updated */
-    private suspend fun downloadPrescriptionPhoto() {
-        fileSyncRepository.startDownload()
+    private suspend fun downloadPrescriptionPhoto(logout: (Boolean, String) -> Unit) {
+        fileSyncRepository.startDownload(logout)
     }
 
     /**
