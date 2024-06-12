596c1d6bf7d9f8356236d61203658c71a7de11cb
mansikalraa <mansi.kalra23@gmail.com>
2024-06-12 12:14:41 +0530

    sync status display update on prescription screen

--- app/src/main/java/com/latticeonfhir/android/data/local/enums/SyncStatusMessageEnum.kt
+++ app/src/main/java/com/latticeonfhir/android/data/local/enums/SyncStatusMessageEnum.kt
@@ -4,5 +4,6 @@ enum class SyncStatusMessageEnum(val message: String, val display: String) {
     SYNCING_IN_PROGRESS("Syncing", "In-progress"),
     SYNCING_FAILED("Sync failed", "Failed"),
     SYNCING_COMPLETED("Sync completed", "Completed"),
-    OFFLINE("Offline", "Offline")
+    OFFLINE("Offline", "Offline"),
+    NO_INTERNET("No internet, data might be outdated", "No internet")
 }
