a7710f9d21737ae0a873b1cba0c23c2eae5ccae4
mansikalraa <mansi.kalra23@gmail.com>
2024-06-21 11:10:23 +0530

    click indication shape to circle

--- app/src/main/java/com/latticeonfhir/android/ui/prescription/photo/upload/PrescriptionPhotoUploadScreen.kt
+++ app/src/main/java/com/latticeonfhir/android/ui/prescription/photo/upload/PrescriptionPhotoUploadScreen.kt
@@ -55,6 +55,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
@@ -453,6 +454,7 @@ private fun BottomRow(
 
         Box(
             modifier = Modifier
+                .clip(CircleShape)
                 .clickable {
                     imageCaptured()
                 },
