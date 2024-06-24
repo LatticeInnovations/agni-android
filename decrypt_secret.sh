f7326e1df7ed2fedd07cd2e8bc07613cf0885483
mansikalraa <mansi.kalra23@gmail.com>
2024-06-24 13:04:39 +0530

    workflow testing

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GOOGLE_SERVICES_PASSPHRASE" \
---output $HOME/secrets/google-services.json google-services.json.gpg
+--output ${PWD}/app/google-services.json ${PWD}/app/google-services.json.gpg
