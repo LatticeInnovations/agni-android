8ea217403449b454eb90065c51a49f9e2dd05f09
mansikalraa <mansi.kalra23@gmail.com>
2024-11-24 14:04:24 +0530

    add to queue dialog added on cvd assessment card click

--- app/src/main/java/com/latticeonfhir/android/ui/patientlandingscreen/PatientLandingScreenViewModel.kt
+++ app/src/main/java/com/latticeonfhir/android/ui/patientlandingscreen/PatientLandingScreenViewModel.kt
@@ -10,12 +10,18 @@ import androidx.work.WorkInfo
 import com.latticeonfhir.android.FhirApp
 import com.latticeonfhir.android.base.viewmodel.BaseAndroidViewModel
 import com.latticeonfhir.android.data.local.enums.AppointmentStatusEnum
+import com.latticeonfhir.android.data.local.model.appointment.AppointmentResponseLocal
 import com.latticeonfhir.android.data.local.repository.appointment.AppointmentRepository
+import com.latticeonfhir.android.data.local.repository.generic.GenericRepository
 import com.latticeonfhir.android.data.local.repository.patient.PatientRepository
+import com.latticeonfhir.android.data.local.repository.patient.lastupdated.PatientLastUpdatedRepository
+import com.latticeonfhir.android.data.local.repository.preference.PreferenceRepository
 import com.latticeonfhir.android.data.local.repository.prescription.PrescriptionRepository
+import com.latticeonfhir.android.data.local.repository.schedule.ScheduleRepository
 import com.latticeonfhir.android.data.server.model.patient.PatientResponse
 import com.latticeonfhir.android.service.workmanager.utils.Sync
 import com.latticeonfhir.android.service.workmanager.workers.trigger.TriggerWorkerPeriodicImpl
+import com.latticeonfhir.android.utils.common.Queries
 import com.latticeonfhir.android.utils.converters.responseconverter.TimeConverter.toEndOfDay
 import com.latticeonfhir.android.utils.converters.responseconverter.TimeConverter.toTodayStartDate
 import com.latticeonfhir.android.utils.network.CheckNetwork
@@ -31,7 +37,11 @@ class PatientLandingScreenViewModel @Inject constructor(
     application: Application,
     private val patientRepository: PatientRepository,
     private val appointmentRepository: AppointmentRepository,
-    private val prescriptionRepository: PrescriptionRepository
+    private val prescriptionRepository: PrescriptionRepository,
+    private val genericRepository: GenericRepository,
+    private val preferenceRepository: PreferenceRepository,
+    private val scheduleRepository: ScheduleRepository,
+    private val patientLastUpdatedRepository: PatientLastUpdatedRepository
 ) : BaseAndroidViewModel(application) {
     var isLaunched by mutableStateOf(false)
     var patient by mutableStateOf<PatientResponse?>(null)
@@ -44,6 +54,12 @@ class PatientLandingScreenViewModel @Inject constructor(
     var pastAppointmentsCount by mutableIntStateOf(0)
     var isFabSelected by mutableStateOf(false)
     var showAllSlotsBookedDialog by mutableStateOf(false)
+
+    var appointment by mutableStateOf<AppointmentResponseLocal?>(null)
+    var canAddAssessment by mutableStateOf(false)
+    var showAddToQueueDialog by mutableStateOf(false)
+    var ifAllSlotsBooked by mutableStateOf(false)
+    private val maxNumberOfAppointmentsInADay = 250
 
     var selectedIndex by mutableIntStateOf(0)
 
@@ -83,6 +99,33 @@ class PatientLandingScreenViewModel @Inject constructor(
         return patientRepository.getPatientById(id)[0]
     }
 
+    internal fun getAppointmentInfo(callback: () -> Unit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            appointment = appointmentRepository.getAppointmentsOfPatientByStatus(
+                patient!!.id,
+                AppointmentStatusEnum.SCHEDULED.value
+            ).firstOrNull { appointmentResponse ->
+                appointmentResponse.slot.start.time < Date().toEndOfDay() && appointmentResponse.slot.start.time > Date().toTodayStartDate()
+            }
+            appointmentRepository.getAppointmentsOfPatientByDate(
+                patient!!.id,
+                Date().toTodayStartDate(),
+                Date().toEndOfDay()
+            ).let { appointmentResponse ->
+                canAddAssessment =
+                    appointmentResponse?.status == AppointmentStatusEnum.ARRIVED.value || appointmentResponse?.status == AppointmentStatusEnum.WALK_IN.value
+                            || appointmentResponse?.status == AppointmentStatusEnum.IN_PROGRESS.value
+            }
+            ifAllSlotsBooked = appointmentRepository.getAppointmentListByDate(
+                Date().toTodayStartDate(),
+                Date().toEndOfDay()
+            ).filter { appointmentResponseLocal ->
+                appointmentResponseLocal.status != AppointmentStatusEnum.CANCELLED.value
+            }.size >= maxNumberOfAppointmentsInADay
+            callback()
+        }
+    }
+
     internal fun getScheduledAppointmentsCount(patientId: String) {
         viewModelScope.launch(Dispatchers.IO) {
             appointmentsCount = appointmentRepository.getAppointmentsOfPatientByStatus(
@@ -101,6 +144,40 @@ class PatientLandingScreenViewModel @Inject constructor(
     internal fun getUploadsCount(patientId: String) {
         viewModelScope.launch(Dispatchers.IO) {
             uploadsCount = prescriptionRepository.getLastPhotoPrescription(patientId).size
+        }
+    }
+
+
+    internal fun addPatientToQueue(patient: PatientResponse, addedToQueue: (List<Long>) -> Unit) {
+        viewModelScope.launch(Dispatchers.IO) {
+            Queries.addPatientToQueue(
+                patient,
+                scheduleRepository,
+                genericRepository,
+                preferenceRepository,
+                appointmentRepository,
+                patientLastUpdatedRepository,
+                addedToQueue
+            )
+        }
+    }
+
+    internal fun updateStatusToArrived(
+        patient: PatientResponse,
+        appointment: AppointmentResponseLocal,
+        updated: (Int) -> Unit
+    ) {
+        viewModelScope.launch(Dispatchers.IO) {
+            Queries.updateStatusToArrived(
+                patient,
+                appointment,
+                appointmentRepository,
+                genericRepository,
+                preferenceRepository,
+                scheduleRepository,
+                patientLastUpdatedRepository,
+                updated
+            )
         }
     }
 }
