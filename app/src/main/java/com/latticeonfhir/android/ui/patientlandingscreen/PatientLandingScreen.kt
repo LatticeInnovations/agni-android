21e52b9969a5aa1bde9120099ba931c0a7f637be
mansikalraa <mansi.kalra23@gmail.com>
2024-11-25 16:01:34 +0530

    navigation bar padding added

--- app/src/main/java/com/latticeonfhir/android/ui/patientlandingscreen/PatientLandingScreen.kt
+++ app/src/main/java/com/latticeonfhir/android/ui/patientlandingscreen/PatientLandingScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -270,6 +271,7 @@ fun PatientLandingScreen(
         SnackbarHost(
             hostState = snackbarHostState,
             modifier = Modifier.padding(bottom = 160.dp)
+                .navigationBarsPadding()
         )
     }
     viewModel.patient?.let { patient ->
