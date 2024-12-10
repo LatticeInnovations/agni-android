58d6158667f045229239f7ecb3a008cfe4cd4018
mansikalraa <mansi.kalra23@gmail.com>
2024-12-10 10:43:15 +0530

    graph msg changed in cvd

--- app/src/main/java/com/latticeonfhir/android/ui/cvd/records/CVDRiskAssessmentRecords.kt
+++ app/src/main/java/com/latticeonfhir/android/ui/cvd/records/CVDRiskAssessmentRecords.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.github.mikephil.charting.charts.LineChart
@@ -64,7 +65,8 @@ fun CVDRiskAssessmentRecords(
                     text = stringResource(R.string.graph_info),
                     style = MaterialTheme.typography.bodyMedium,
                     color = MaterialTheme.colorScheme.outline,
-                    modifier = Modifier.padding(20.dp)
+                    modifier = Modifier.padding(20.dp),
+                    textAlign = TextAlign.Center
                 )
             }
         } else {
