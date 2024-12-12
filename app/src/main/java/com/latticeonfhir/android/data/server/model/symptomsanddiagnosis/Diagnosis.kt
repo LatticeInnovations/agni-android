d5712c14ffeb6ee391d89555913fb6c86574c7a4
zibran <zibran@thelattice.in>
2024-12-12 09:33:57 +0530

    diagnosis checkbox selection issue fixed and removed unused import

--- app/src/main/java/com/latticeonfhir/android/data/server/model/symptomsanddiagnosis/Diagnosis.kt
+++ app/src/main/java/com/latticeonfhir/android/data/server/model/symptomsanddiagnosis/Diagnosis.kt
@@ -1,7 +1,6 @@
 package com.latticeonfhir.android.data.server.model.symptomsanddiagnosis
 
 import androidx.annotation.Keep
-import com.latticeonfhir.android.data.server.model.symptomsanddiagnosis.SymptomsAndDiagnosisItem
 
 @Keep
 data class Diagnosis(
