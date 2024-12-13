795c0b62ed789978575a393a66fc055ee5c840b8
mansikalraa <mansi.kalra23@gmail.com>
2024-12-13 12:56:57 +0530

    navigation bar padding in form prescription

--- app/src/main/java/com/latticeonfhir/android/ui/prescription/PrescriptionScreen.kt
+++ app/src/main/java/com/latticeonfhir/android/ui/prescription/PrescriptionScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -300,7 +301,9 @@ fun BottomNavLayout(
         enter = expandVertically(),
         exit = shrinkVertically()
     ) {
-        Column {
+        Column(
+            modifier = Modifier.navigationBarsPadding()
+        ) {
             AnimatedVisibility(viewModel.bottomNavExpanded) {
                 Surface(
                     color = MaterialTheme.colorScheme.surface,
