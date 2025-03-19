223d1e18e4f452797d95849269755934b0ad8ea4
Hâwk <naveen.ece.1728@iiitbh.ac.in>
2025-03-19 15:25:08 +0530

    https://github.com/LatticeInnovations/LatticeOnFhirMain/issues/946

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.test) apply false
+    alias(libs.plugins.baselineprofile) apply false
+    alias(libs.plugins.compose) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.dependencyGuard) apply false
+    alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.firebase.perf) apply false
+    alias(libs.plugins.gms) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.roborazzi) apply false
+    alias(libs.plugins.secrets) apply false
+    alias(libs.plugins.room) apply false
+    alias(libs.plugins.module.graph) apply true // Plugin applied to allow module graph generation
+    alias(libs.plugins.sonarqube) apply false
+}
+
+//sonar {
+//    properties {
+//        property "sonar.projectKey", "LatticeInnovations_FHIR-Android_AZAvReGoi3XM6MLXDS_L"
+//        property "sonar.projectName", "FHIR-Android"
+//        property "sonar.scm.provider", "git"
+//    }
+//}
