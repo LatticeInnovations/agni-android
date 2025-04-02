89261afbc0b90dae0e91e42ac4fccbdd8af72d75
mansikalraa <mansi.kalra23@gmail.com>
2025-04-02 12:51:13 +0530

    created repository module

---
+++ features/auth/src/main/java/com/latticeonfhir/android/auth/di/RepositoryModule.kt
@@ -0,0 +1,24 @@
+package com.latticeonfhir.android.auth.di
+
+import com.latticeonfhir.android.auth.data.server.repository.authentication.AuthenticationRepository
+import com.latticeonfhir.android.auth.data.server.repository.authentication.AuthenticationRepositoryImpl
+import com.latticeonfhir.android.auth.data.server.repository.signup.SignUpRepository
+import com.latticeonfhir.android.auth.data.server.repository.signup.SignUpRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+
+
+@Module
+@InstallIn(ViewModelComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @ViewModelScoped
+    abstract fun provideAuthenticationRepository(authenticationRepositoryImpl: AuthenticationRepositoryImpl): AuthenticationRepository
+
+    @Binds
+    @ViewModelScoped
+    abstract fun provideSignUpRepository(signUpRepositoryImpl: SignUpRepositoryImpl): SignUpRepository
+}
