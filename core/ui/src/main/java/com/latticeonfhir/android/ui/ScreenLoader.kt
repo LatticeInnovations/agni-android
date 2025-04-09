4019b7506006212e3b94d616bf76051e6e45dac5
mansikalraa <mansi.kalra23@gmail.com>
2025-04-09 15:15:52 +0530

    prescription feature module

---
+++ core/ui/src/main/java/com/latticeonfhir/android/ui/ScreenLoader.kt
@@ -0,0 +1,67 @@
+package com.latticeonfhir.android.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+
+@Composable
+fun ScreenLoader(
+    text: String = stringResource(R.string.saving)
+) {
+    Box(contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .zIndex(4f)
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.outline.copy(alpha = 0.5f))
+            .clickable(enabled = false) { }
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(40.dp),
+            shape = RoundedCornerShape(24.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(
+                    8.dp
+                )
+            )
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 17.dp, bottom = 17.dp, start = 24.dp, end = 24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                ButtonLoader(width = 48.dp, height = 48.dp)
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.titleSmall,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 24.dp),
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+}
