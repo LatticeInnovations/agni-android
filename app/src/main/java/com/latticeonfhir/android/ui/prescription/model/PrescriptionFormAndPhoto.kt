a3c8d9797aced0e6fd0aee6861ae811192300820
mansikalraa <mansi.kalra23@gmail.com>
2025-04-09 15:15:52 +0530

    prescription feature module

--- app/src/main/java/com/latticeonfhir/android/ui/prescription/model/PrescriptionFormAndPhoto.kt
+++
@@ -1,9 +0,0 @@
-package com.latticeonfhir.android.ui.prescription.model
-
-import java.util.Date
-
-data class PrescriptionFormAndPhoto(
-    val date: Date,
-    val type: String,
-    val prescription: Any
-)
