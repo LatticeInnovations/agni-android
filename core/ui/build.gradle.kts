8e6789b3b97af85b09f44b92cde11c8c57f9c3af
mansikalraa <mansi.kalra23@gmail.com>
2025-05-04 18:35:14 +0530

    appointments fab added in ui module

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -12,14 +12,17 @@ dependencies {
     api(projects.core.model)
     api(projects.core.utils)
     api(projects.core.theme)
+    api(projects.core.navigation)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.runtime.android)
     implementation(libs.androidx.material3.android)
+    implementation(libs.androidx.navigation.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.test.espresso.core)
     implementation(libs.androidx.compose.material.iconsExtended)
+    implementation(libs.androidx.hilt.navigation.compose)
 }
