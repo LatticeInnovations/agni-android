ec2a9ef623f37096e281fff007c4b4a90165602c
mansikalraa <mansi.kalra23@gmail.com>
2025-07-09 15:43:26 +0530

    patient registration step 2 ui update

--- app/src/main/java/com/heartcare/agni/ui/patientregistration/step2/PatientRegistrationStepTwo.kt
+++ app/src/main/java/com/heartcare/agni/ui/patientregistration/step2/PatientRegistrationStepTwo.kt
@@ -1,6 +1,5 @@
 package com.heartcare.agni.ui.patientregistration.step2
 
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,28 +7,34 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.heartcare.agni.R
-import com.heartcare.agni.ui.common.CustomTextField
-import com.heartcare.agni.ui.common.IdLength
-import com.heartcare.agni.ui.common.IdSelectionChip
+import com.heartcare.agni.ui.common.CustomTextFieldWithLength
 import com.heartcare.agni.ui.patientregistration.PatientRegistrationViewModel
 import com.heartcare.agni.ui.patientregistration.model.PatientRegister
 import com.heartcare.agni.ui.theme.Neutral40
+import com.heartcare.agni.utils.regex.OnlyNumberRegex.onlyNumbers
+import com.heartcare.agni.utils.regex.RegexPatterns.atLeastOneAlphaAndNumber
 
 @Composable
 fun PatientRegistrationStepTwo(
@@ -59,7 +64,7 @@ fun PatientRegistrationStepTwo(
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Text(
-                text = stringResource(id = R.string.optional_identification),
+                text = stringResource(id = R.string.identification),
                 style = MaterialTheme.typography.titleMedium,
                 color = MaterialTheme.colorScheme.onSurface
             )
@@ -73,38 +78,11 @@ fun PatientRegistrationStepTwo(
         Column(
             modifier = Modifier
                 .weight(1f)
-                .verticalScroll(rememberScrollState())
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
         ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .horizontalScroll(rememberScrollState())
-            ) {
-                IdSelectionChip(
-                    idSelected = viewModel.isPassportSelected,
-                    label = stringResource(id = R.string.passport_id)
-                ) {
-                    viewModel.isPassportSelected = !it
-                }
-                Spacer(modifier = Modifier.width(5.dp))
-                IdSelectionChip(
-                    idSelected = viewModel.isVoterSelected,
-                    label = stringResource(id = R.string.voter_id)
-                ) {
-                    viewModel.isVoterSelected = !it
-                }
-                Spacer(modifier = Modifier.width(5.dp))
-                IdSelectionChip(
-                    idSelected = viewModel.isPatientSelected,
-                    label = stringResource(id = R.string.patient_id)
-                ) {
-                    viewModel.isPatientSelected = !it
-                }
-            }
-            Spacer(modifier = Modifier.height(10.dp))
-            PassportIdComposable(viewModel)
-            VoterIdComposable(viewModel)
-            PatientIdComposable(viewModel)
+            HospitalIdComposable(viewModel)
+            NationalIdComposable(viewModel)
         }
         Button(
             onClick = {
@@ -119,87 +97,135 @@ fun PatientRegistrationStepTwo(
                 .fillMaxWidth(),
             enabled = viewModel.identityInfoValidation()
         ) {
-            Text(text = "Next")
+            Text(text = stringResource(R.string.next))
         }
     }
 }
 
 @Composable
-private fun PatientIdComposable(viewModel: PatientRegistrationStepTwoViewModel) {
-    Column {
-        if (viewModel.isPatientSelected) {
-            Spacer(modifier = Modifier.height(5.dp))
-            CustomTextField(
-                value = viewModel.patientId,
-                label = stringResource(id = R.string.patient_id),
-                weight = 1f,
-                viewModel.maxPatientIdLength,
-                viewModel.isPatientValid,
-                stringResource(id = R.string.patient_id_error_msg),
-                KeyboardType.Text,
-                KeyboardCapitalization.Characters
-            ) {
-                viewModel.patientId = it
-                viewModel.isPatientValid =
-                    viewModel.patientId.length < viewModel.minPatientIdLength
-            }
-            IdLength(viewModel.patientId, viewModel.maxPatientIdLength, "PATIENT_ID_LENGTH")
-        } else {
-            viewModel.patientId = ""
-            viewModel.isPatientValid = false
+private fun HospitalIdComposable(
+    viewModel: PatientRegistrationStepTwoViewModel
+) {
+    CustomTextFieldWithLength(
+        value = viewModel.hospitalId,
+        label = stringResource(id = R.string.hospital_id),
+        placeholder = null,
+        weight = 1f,
+        maxLength = viewModel.maxHospitalIdLength,
+        isError = viewModel.isHospitalIdValid,
+        error = stringResource(id = R.string.hospital_id_error_msg),
+        keyboardType = KeyboardType.Text,
+        keyboardCapitalization = KeyboardCapitalization.Characters
+    ) { value ->
+        val filtered = value.filter { it.isLetterOrDigit() }
+        if (value.length <= viewModel.maxHospitalIdLength && (value == filtered || value.isEmpty()))
+            viewModel.hospitalId = value
+        viewModel.isHospitalIdValid = viewModel.hospitalId.isNotBlank() &&
+                !atLeastOneAlphaAndNumber.matches(viewModel.hospitalId)
+    }
+}
+
+@Composable
+private fun NationalIdComposable(
+    viewModel: PatientRegistrationStepTwoViewModel
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        OutlinedTextField(
+            value = viewModel.nationalId,
+            onValueChange = {
+                if (it.length <= viewModel.maxNationalIdLength && (it.matches(onlyNumbers) || it.isEmpty())) {
+                    viewModel.nationalId = it
+                    viewModel.isVerifyClicked = false
+                    viewModel.isNationalIdVerified = false
+                }
+            },
+            label = {
+                Text(stringResource(R.string.national_id))
+            },
+            modifier = Modifier.weight(3f),
+            supportingText = {
+                NationalIdSupportingText(viewModel)
+            },
+            trailingIcon = {
+                if(viewModel.nationalId.isNotBlank())
+                    IconButton(
+                        onClick = {
+                            viewModel.nationalId = ""
+                            viewModel.isVerifyClicked = false
+                            viewModel.isNationalIdVerified = false
+                        }
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.cancel),
+                            contentDescription = null
+                        )
+                    }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number
+            )
+        )
+        Button(
+            onClick = {
+                viewModel.isVerifyClicked = true
+                // TODO : add logic to verify national id
+                viewModel.isNationalIdVerified = false
+            },
+            modifier = Modifier.weight(1f),
+            enabled = viewModel.nationalId.isNotBlank() && !viewModel.isNationalIdVerified
+        ) {
+            Text(stringResource(R.string.verify))
         }
     }
 }
 
 @Composable
-private fun VoterIdComposable(viewModel: PatientRegistrationStepTwoViewModel) {
-    Column {
-        if (viewModel.isVoterSelected) {
-            Spacer(modifier = Modifier.height(5.dp))
-            CustomTextField(
-                value = viewModel.voterId,
-                label = stringResource(id = R.string.voter_id),
-                weight = 1f,
-                viewModel.maxVoterIdLength,
-                viewModel.isVoterValid,
-                stringResource(id = R.string.voter_id_error_msg),
-                KeyboardType.Text,
-                KeyboardCapitalization.Characters
-            ) {
-                viewModel.voterId = it
-                viewModel.isVoterValid = !viewModel.voterPattern.matches(viewModel.voterId)
+private fun NationalIdSupportingText(
+    viewModel: PatientRegistrationStepTwoViewModel
+) {
+    Row (
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        if (viewModel.isVerifyClicked) {
+            val text: String
+            val icon: Int
+            val color: Color
+            if (viewModel.isNationalIdVerified) {
+                text = stringResource(R.string.verified)
+                icon = R.drawable.sync_completed_icon
+                color = MaterialTheme.colorScheme.primary
+            } else {
+                text = stringResource(R.string.unverified)
+                icon = R.drawable.info
+                color = MaterialTheme.colorScheme.onSurfaceVariant
             }
-            IdLength(viewModel.voterId, viewModel.maxVoterIdLength, "VOTER_ID_LENGTH")
-        } else {
-            viewModel.voterId = ""
-            viewModel.isVoterValid = false
-        }
-    }
-}
-
-@Composable
-private fun PassportIdComposable(viewModel: PatientRegistrationStepTwoViewModel) {
-    Column {
-        if (viewModel.isPassportSelected) {
-            Spacer(modifier = Modifier.height(5.dp))
-            CustomTextField(
-                value = viewModel.passportId,
-                label = stringResource(id = R.string.passport_id),
-                weight = 1f,
-                viewModel.maxPassportIdLength,
-                viewModel.isPassportValid,
-                stringResource(id = R.string.passport_id_error_msg),
-                KeyboardType.Text,
-                KeyboardCapitalization.Characters
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(4.dp)
             ) {
-                viewModel.passportId = it
-                viewModel.isPassportValid =
-                    !viewModel.passportPattern.matches(viewModel.passportId)
+                Icon(
+                    painter = painterResource(icon),
+                    contentDescription = null,
+                    tint = color,
+                    modifier = Modifier.size(16.dp)
+                )
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.bodySmall,
+                    color = color
+                )
             }
-            IdLength(viewModel.passportId, viewModel.maxPassportIdLength, "PASSPORT_ID_LENGTH")
-        } else {
-            viewModel.passportId = ""
-            viewModel.isPassportValid = false
         }
+        Spacer(modifier = Modifier.weight(1f))
+        Text(
+            text = "${viewModel.nationalId.length}/${viewModel.maxNationalIdLength}",
+            style = MaterialTheme.typography.bodySmall,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
     }
 }
--- app/src/main/java/com/heartcare/agni/ui/patientregistration/step2/PatientRegistrationStepTwoViewModel.kt
+++ app/src/main/java/com/heartcare/agni/ui/patientregistration/step2/PatientRegistrationStepTwoViewModel.kt
@@ -9,10 +9,8 @@ import com.heartcare.agni.base.viewmodel.BaseViewModel
 class PatientRegistrationStepTwoViewModel : BaseViewModel(), DefaultLifecycleObserver {
     var isLaunched by mutableStateOf(false)
 
-    val maxPassportIdLength = 8
-    val maxVoterIdLength = 10
-    val maxPatientIdLength = 12
-    val minPatientIdLength = 2
+    val maxHospitalIdLength = 6
+    val maxNationalIdLength = 6
 
     var isPassportSelected by mutableStateOf(false)
     var isVoterSelected by mutableStateOf(false)
@@ -20,18 +18,15 @@ class PatientRegistrationStepTwoViewModel : BaseViewModel(), DefaultLifecycleObs
     var passportId by mutableStateOf("")
     var voterId by mutableStateOf("")
     var patientId by mutableStateOf("")
-
-    val passportPattern = Regex("^[A-PR-WYa-pr-wy][1-9]\\d\\s?\\d{4}[1-9]$")
-    val voterPattern = Regex("^[A-Za-z]{3}[0-9]{7}$")
-    var isPassportValid by mutableStateOf(false)
-    var isVoterValid by mutableStateOf(false)
-    var isPatientValid by mutableStateOf(false)
+    var hospitalId by mutableStateOf("")
+    var nationalId by mutableStateOf("")
+    var isVerifyClicked by mutableStateOf(false)
+    var isNationalIdVerified by mutableStateOf(false)
+    var isHospitalIdValid by mutableStateOf(false)
 
     fun identityInfoValidation(): Boolean {
-        if (isPassportSelected && !passportPattern.matches(passportId))
+        if (isHospitalIdValid)
             return false
-        if (isVoterSelected && !voterPattern.matches(voterId))
-            return false
-        return !(isPatientSelected && patientId.length < minPatientIdLength)
+        return isVerifyClicked || nationalId.isBlank()
     }
 }
