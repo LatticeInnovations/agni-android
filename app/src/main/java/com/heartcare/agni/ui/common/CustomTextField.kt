276378c079fab0d4cf3134e5e3b5a4881535aed5
mansikalraa <mansi.kalra23@gmail.com>
2025-07-09 15:12:43 +0530

    patient registration step 1 ui update

--- app/src/main/java/com/heartcare/agni/ui/common/CustomTextField.kt
+++ app/src/main/java/com/heartcare/agni/ui/common/CustomTextField.kt
@@ -1,5 +1,7 @@
 package com.heartcare.agni.ui.common
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
@@ -51,6 +53,67 @@ fun CustomTextField(
         isError = isError,
         supportingText = {
             if (isError) Text(text = error, style = MaterialTheme.typography.bodySmall)
+        }
+    )
+}
+
+@Composable
+fun CustomTextFieldWithLength(
+    value: String,
+    label: String? = null,
+    placeholder: String? = null,
+    weight: Float,
+    maxLength: Int,
+    isError: Boolean,
+    error: String,
+    keyboardType: KeyboardType,
+    keyboardCapitalization: KeyboardCapitalization,
+    updateValue: (String) -> Unit
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = {
+            if (it.length <= maxLength)
+                updateValue(it)
+        },
+        modifier = Modifier
+            .fillMaxWidth(weight),
+        label = if (label == null) null else {
+            {
+                Text(
+                    text = label,
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+        },
+        placeholder = if (placeholder == null) null else {
+            {
+                Text(
+                    text = placeholder,
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+        },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = keyboardType,
+            capitalization = keyboardCapitalization
+        ),
+        isError = isError,
+        supportingText = {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                if (isError) Text(text = error, style = MaterialTheme.typography.bodySmall)
+                Spacer(modifier = Modifier.weight(1f))
+                Text(
+                    text = "${value.length}/$maxLength",
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
         }
     )
 }
