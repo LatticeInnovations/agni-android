186663de08c1262b6b0e0380c8c3950f34077133
Mansi Kalra <mansi.kalra23@gmail.com>
2026-06-19 18:34:40 +0530

    updated json

--- app/src/main/java/com/latticeonfhir/android/data/local/model/states/District.kt
+++ app/src/main/java/com/latticeonfhir/android/data/local/model/states/District.kt
@@ -8,5 +8,7 @@ data class District(
     val blocks: List<Block>,
     @SerializedName("district_name")
     val districtName: String,
+    @SerializedName("district_code")
+    val districtCode: String,
     val pincodes: List<String>
 )
